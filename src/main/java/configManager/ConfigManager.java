0fbcd6f9c5a6fac036a7e54a683268909b216bef
Babithanjali <b.gopalaswamy@bgopalaswamy-mbpr.fireeye.com>
2017-03-21 12:33:02 -0700

    new updates

--- src/main/java/configManager/ConfigManager.java
+++ src/main/java/configManager/ConfigManager.java
@@ -1921,7 +1921,7 @@ public class ConfigManager {
 					}
 				}
 				
-				//concatenate regions string not required testing ConfigVersion1 branch
+				//concatenate regions string not required testing ConfigVersion1 branch, testing two branch switching
 				String newRegionStr = "";
 				for(int arrIndex=0;arrIndex<newRegions.size();arrIndex++){
 					newRegionStr += newRegions.get(arrIndex) + ",";
